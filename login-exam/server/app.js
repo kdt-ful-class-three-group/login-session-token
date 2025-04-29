5cbf6f690a509cbbc9d1bbad47d81da85d10064c
전선일 <jeonsunil1225@gamil.com>
2025-04-29 17:59:49 +0900

    최정의 코드 중에서 채택할 것을 하고, index.html을 불러와, html에서 session스토리지에 저장하는 과정. sessionStorage는 프론트에서 활용해야 함.

--- login-exam/server/app.js
+++ login-exam/server/app.js
@@ -12,10 +12,12 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(
 	session({
-		secret: '비밀키', // 세션 암호화에 사용되는 키
+		secret: 'adfadlfjalkfjdjasflkjadsjf', // 세션 암호화에 사용되는 키
 		resave: false, // 요청이 왔을 때 세션에 변화가 없으면 저장 안 함
 		saveUninitialized: false, // 초기화되지 않은 세션 저장 안 함
 		cookie: {
+      secure: false,
+      httpOnly: true,
 			maxAge: 1000 * 60 * 60, // 쿠키 만료 시간 (1시간)
 		},
 	})
@@ -25,18 +27,38 @@ app.use(
 app.get('/', (req, res) => {
   res.send(fs.readFileSync('index.html', 'utf8'));
 });
-
 // Login route
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
+   req.session.user = {
+    username: username,
+    password: password
+   }
+   console.log("세션에 저장된 사용자 정보:", req.session.user);
 
   // Simple validation logic
-  if (username === '' && password === '') {
-    res.json({ message: 'Login successful!' });
+  if (username === 'id' && password === 'pw') {
+    res.json({ message: 'Login successful!', username: username });
   } else {
     res.status(401).json({ message: 'Invalid credentials' });
   }
 });
+
+app.get("/api/session",(req,res) => {
+
+      if(req.session.user){
+          res.json({
+                      loggedIn: true,
+                      user: req.session.user.username
+                    })
+                  
+      } else{
+            res.status(401).json({
+                loggedIn: false,
+                msg:"세션이 없습니다."
+              })
+             }
+            })
 
 // Start the server
 app.listen(PORT, () => {
