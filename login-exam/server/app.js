a9ed96413833de9d2619880b67db1c68c68247c8
전선일 <jeonsunil1225@gamil.com>
2025-04-29 14:59:33 +0900

    html파일 연결 및 구성

--- login-exam/server/app.js
+++ login-exam/server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fs = require('fs');
+const session = require('express-session');
 
 const app = express();
 const PORT = 8000;
@@ -8,10 +10,20 @@ const PORT = 8000;
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
+app.use(
+	session({
+		secret: '비밀키', // 세션 암호화에 사용되는 키
+		resave: false, // 요청이 왔을 때 세션에 변화가 없으면 저장 안 함
+		saveUninitialized: false, // 초기화되지 않은 세션 저장 안 함
+		cookie: {
+			maxAge: 1000 * 60 * 60, // 쿠키 만료 시간 (1시간)
+		},
+	})
+);
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Server is running on port 8000');
+  res.send(fs.readFileSync('index.html', 'utf8'));
 });
 
 // Login route
